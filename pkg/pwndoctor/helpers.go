ba3f12457ab7f92878096378c6c71f676c27753d
bc0la <michaelcoladougherty@gmail.com>
2024-04-02 18:23:26 -0400

    more cleanup

--- pkg/pwndoctor/helpers.go
+++ pkg/pwndoctor/helpers.go
@@ -390,16 +390,3 @@ func GetAuditNames() []string {
 	}
 	return auditNames
 }
-
-// func CreateAudit(newAuditName string, auditLanguage string, auditType string) {
-// 	// audit := pwndoc.APICreateAudit{
-// 	// 	Name:    newAuditName,
-// 	// 	Language: auditLanguage,
-// 	// 	AuditType: auditType,
-// 	// }
-
-// 	_, err := pwndocAPI.CreateAudit(audit)
-// 	if err != nil {
-// 		log.Fatalf("Error creating audit: %s", err)
-// 	}
-// }
